fd055c90a347749dee13ff00e441a6ec511adf0a
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2020-02-13 22:15:34 -0600

    deleted broken button

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -10,8 +10,7 @@ function HomePage(props) {
         TTRPG games, save them for later, and view maps others have made public.
       </h2>
       <h3>Want to proceed? Adventure awaits.</h3>
-      <a href="/play">button</a>
-      {props.children}
+      {/* {props.children} */}
     </div>
   );
 }
